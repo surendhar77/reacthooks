c81a20d4100b80e5e5cfc8b8ee15f9464a0d75ce
Surennddhar <surendharjeyakumar@gmail.com>
2025-06-17 13:57:59 +0530

    install react icons and styled components

--- src/components/usestate/UseState.jsx
+++ src/components/usestate/UseState.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { BiPlusMedical } from 'react-icons/bi';
+import { FaMinus } from 'react-icons/fa';
 
 const UseState = () => {
   return (
