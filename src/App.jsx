28773f23a99a538143be369357d4a817e62913b5
Surennddhar <surendharjeyakumar@gmail.com>
2025-07-15 16:29:28 +0530

    hook commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 
 import './App.css'
-import ClearUp from './components/useEffect/ClearUp'
-import UseEffect from './components/useEffect/UseEffect'
+// import ClearUp from './components/useeffect/ClearUp'
+// import UseEffect from './components/useeffect/UseEffect'
 // import UseState from './components/usestate/UseState'
 import UseStateObject from './components/usestate/UseStateObject'
+import Parent from './Parent'
+import ReducerHook from './components/usereducer/ReducerHook'
+import RefHook1 from './components/useref/RefHook1'
+import LayoutEffect from './components/uselayouteffect/LayoutEffect'
+import MemoHook from './components/usememo/MemoHook'
+import Callbackhook from './components/usecallback/CallbackHook'
 
 function App() {
 
@@ -12,7 +18,14 @@ function App() {
       {/* <UseState /> */}
       {/* <UseStateObject /> */}
       {/* <UseEffect /> */}
-      \<ClearUp />
+      {/* <ClearUp /> */}
+      {/* <Parent />  */}
+      {/* <ReducerHook /> */}
+      {/* <RefHook /> */}
+ {/* <RefHook1 /> */}
+ {/* <LayoutEffect /> */}
+ {/* <MemoHook /> */}
+ <Callbackhook />
     </>
   )
 }
